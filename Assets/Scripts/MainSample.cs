9f02a0d6b0d0d62e1dfef4f60b2c7a1d855d1141
necotarou <necotarou@gmail.com>
2017-11-12 10:33:56 +0900

    【初期登録】 Jsonを用いるセーブ・ロード

---
+++ Assets/Scripts/MainSample.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSample : MonoBehaviour {
+
+	// データ格納先
+	UserData userData = new UserData();
+
+	protected void Start () {
+
+		// セーブ
+		Save();
+
+
+		// ロード
+		Load();
+	}
+
+
+
+	// セーブ
+	private void Save()
+	{
+		// サンプルデータ代入
+		userData.Hp = 100;
+		userData.Mp = 200;
+		userData.StringList = new List<string>();
+		userData.StringList.Add("CS");
+		userData.StringList.Add("JavaScript");
+		userData.StringList.Add("Boo");
+
+		// セーブ
+		string saveJson = userData.Save(userData);
+		PlayerPrefs.SetString("Data", saveJson);
+
+		Debug.Log(string.Format("Save {0}", saveJson));
+	}
+
+
+	// ロード
+	private void Load()
+	{
+		// ロード
+		string json = PlayerPrefs.GetString("Data", string.Empty);
+		UserData loadData = userData.Load(json);
+
+		// データ表示
+		Debug.Log(string.Format("Hp = {0}", userData.Hp));
+		Debug.Log(string.Format("Mp = {0}", userData.Mp));
+		foreach (string data in userData.StringList)
+		{
+			Debug.Log(string.Format("StringList = {0}", data));
+		}
+	}
+
+
+	protected void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UserData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData {
+
+	// 保存データ
+	public int Hp;
+	public int Mp;
+
+	public List<string> StringList;
+
+
+
+	// データセーブ
+	public string Save(UserData data)
+	{
+		string json = JsonUtility.ToJson(data);
+
+		return json;
+	}
+
+	// データロード
+	public UserData Load(string json)
+	{
+		UserData userData = JsonUtility.FromJson<UserData>(json);
+
+		return userData;
+	}
+
+}
